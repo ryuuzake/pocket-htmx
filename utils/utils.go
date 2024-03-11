c9fe2abd22782bfae721598fa8f9a99f416c76dc
Riza Nafis <riza.nafis@gmail.com>
2024-03-11 13:11:28 +0700

    add htmx.min.js and create button components

--- utils/utils.go
+++ utils/utils.go
@@ -5,29 +5,10 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v5"
-	"github.com/pocketbase/dbx"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/models"
-	"github.com/pocketbase/pocketbase/resolvers"
-	"github.com/pocketbase/pocketbase/tools/search"
 )
 
 func Render(cmp templ.Component, c echo.Context) error {
 	return cmp.Render(c.Request().Context(), c.Response().Writer)
-}
-
-func NewRuleQuery(app core.App, c echo.Context, coll models.Collection, rule string) (*dbx.SelectQuery, error) {
-	info := apis.RequestInfo(c)
-	query := app.Dao().RecordQuery(coll.Name)
-	resolver := resolvers.NewRecordFieldResolver(app.Dao(), &coll, info, true)
-	expr, err := search.FilterData(rule).BuildExpr(resolver)
-	if err != nil {
-		return nil, apis.NewBadRequestError("", err)
-	}
-	resolver.UpdateQuery(query)
-	query.AndWhere(expr)
-	return query, nil
 }
 
 func SetRequestContextValue(c echo.Context, key string, val any) {
